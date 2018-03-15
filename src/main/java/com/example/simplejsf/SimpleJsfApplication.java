0c594bf1e0e1e9ca2df3438eee21311458121e6b
Philip Gutierrez <pgutierrez@owens.com>
2018-03-14 21:28:44 -0400

    initial commit

---
+++ src/main/java/com/example/simplejsf/SimpleJsfApplication.java
@@ -0,0 +1,12 @@
+package com.example.simplejsf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleJsfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleJsfApplication.class, args);
+	}
+}
